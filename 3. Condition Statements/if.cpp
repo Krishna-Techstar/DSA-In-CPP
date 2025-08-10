14f0c1705db8d701622a197711a11fedb33d0b65
Krishnakant Sharma <139650964+Krishna-Techstar@users.noreply.github.com>
2025-08-11 01:22:46 +0530

    Conditional Statements Started

---
+++ 3. Condition Statements/if.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=-5;
+    if(n>=0){
+        cout<<"positive";
+    }
+
+    return 0;
+}
---
+++ 3. Condition Statements/ifelse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+    int n;
+    cout<<"please Enter No You Wanna Check \n";
+    cin>> n;
+    if(n>=0){
+        cout<<"N is a Positive Number";
+
+    }else{
+        cout<<"N is a Negitive Number";
+    }
+    return 0;
+ }
